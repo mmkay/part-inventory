d627789cf5b73a1f5232c8b4fc5fbf2876610d21
Mateusz Kulewicz <mateusz.kulewicz@gmail.com>
2018-02-04 12:45:40 +0100

    Get part by UUID

--- src/main/java/pl/mmkay/inventory/part/PartController.java
+++ src/main/java/pl/mmkay/inventory/part/PartController.java
@@ -2,10 +2,12 @@ package pl.mmkay.inventory.part;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/part")
@@ -16,5 +18,10 @@ public class PartController {
     @GetMapping("all")
     public List<Part> getAllParts() {
         return (List<Part>) partRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Part getPartById(@PathVariable("id") UUID uuid) {
+        return partRepository.findOne(uuid);
     }
 }
