528d2e411a3112c4e706fa5ca7337b14c9c9d511
Mateusz Kulewicz <mateusz.kulewicz@gmail.com>
2018-02-05 18:37:29 +0100

    Enable CORS throughout app

--- src/main/java/pl/mmkay/inventory/InventoryApplication.java
+++ src/main/java/pl/mmkay/inventory/InventoryApplication.java
@@ -2,7 +2,11 @@ package pl.mmkay.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableJpaRepositories
@@ -10,5 +14,15 @@ public class InventoryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
 	}
 }
